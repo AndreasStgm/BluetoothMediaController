1d4bd0aab0833a846d6fdbef93593547f1ad1075
Andreas <andreassteegmans@hotmail.com>
2025-04-05 10:02:56 +0200

    Added basic blinking when not connected

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ Uses the [BLEKeyboard library](https://github.com/T-vK/ESP32-BLE-Keyboard).
 |     Red Flexible Wire (1mm)     |  € 0.14   | 4 x 20cm (5m) |   € 3.50   |          https://www.gotron.be/soepele-montagedraad-5m-rood-1mm.html           |
 |    Black Flexible Wire (1mm)    |  € 0.14   | 4 x 20cm (5m) |   € 3.50   |          https://www.gotron.be/soepele-montagedraad-5m-zwart-1mm.html          |
 |         ESP32 Dev Board         |  € 15.90  |       1       |  € 15.90   |                 https://www.gotron.be/nodemcu-esp32-joyit.html                 |
+|       Test PCB (50x100mm)       |  € 1.15   |       1       |   € 1.15   |    https://www.gotron.be/testprint-50x100mm-met-halfdoorlopende-banen.html     |
--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,56 @@
 
 BleKeyboard keyboardInstance;
 
+const uint8_t BUTTON_1 = 34;
+const uint8_t BUTTON_2 = 35;
+const uint8_t BUTTON_1_LED = 32;
+const uint8_t BUTTON_2_LED = 33;
+
+uint16_t delayCounter = 0;
+
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("Starting BLE Keyboard instance.");
+    // Configuring the pins as in- and outputs
+    pinMode(BUTTON_1, INPUT_PULLUP);
+    pinMode(BUTTON_2, INPUT_PULLUP);
+    pinMode(BUTTON_1_LED, OUTPUT);
+    pinMode(BUTTON_2_LED, OUTPUT);
+
     keyboardInstance.begin();
+
+    // Turning on the LED's to indicate startup
+    digitalWrite(BUTTON_1_LED, HIGH);
+    digitalWrite(BUTTON_2_LED, HIGH);
 }
 
 void loop()
 {
     if (keyboardInstance.isConnected())
     {
-        Serial.println("<BLEKeyboard> Writing play/pause press.");
-        keyboardInstance.write(KEY_MEDIA_PLAY_PAUSE);
+        // Turn off the LEDs when connected
+        delayCounter = 0;
+        digitalWrite(BUTTON_1_LED, LOW);
+        digitalWrite(BUTTON_2_LED, LOW);
 
-        keyboardInstance.write(KEY_MEDIA_NEXT_TRACK);
-        keyboardInstance.write(KEY_MEDIA_PREVIOUS_TRACK);
-        keyboardInstance.write(KEY_MEDIA_VOLUME_UP);
-        keyboardInstance.write(KEY_MEDIA_VOLUME_DOWN);
+        // Serial.println("<BLEKeyboard> Writing play/pause press.");
+        // keyboardInstance.write(KEY_MEDIA_PLAY_PAUSE);
+
+        // keyboardInstance.write(KEY_MEDIA_NEXT_TRACK);
+        // keyboardInstance.write(KEY_MEDIA_PREVIOUS_TRACK);
+        // keyboardInstance.write(KEY_MEDIA_VOLUME_UP);
+        // keyboardInstance.write(KEY_MEDIA_VOLUME_DOWN);
     }
+    else
+    {
+        if (delayCounter >= 1000)
+        {
+            // Flash the LEDs on and off every second to indicate unconnected state
+            delayCounter = 0;
+            digitalWrite(BUTTON_1_LED, !digitalRead(BUTTON_1_LED));
+            digitalWrite(BUTTON_2_LED, !digitalRead(BUTTON_2_LED));
+        }
+    }
+
+    delayCounter++;
+    delay(1);
 }
